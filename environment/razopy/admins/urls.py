9796928c199d8b80f900d19940d9b8e7c4adddc2
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-04-11 09:05:31 +0530

    Search functionality for user control

--- environment/razopy/admins/urls.py
+++ environment/razopy/admins/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin-wallet/', views.admin_wallet, name='admin_wallet'),
     path('admin-base/',views.admin_base, name='admin_base'),
     path('admin-logout/', views.admin_logout, name='admin_logout'),
-    path('', views.admin_login, name='admin_login')
+    path('', views.admin_login, name='admin_login'),
+    path('admin-home/block_user/<int:user_id>/', views.block_user, name='block_user'),
+    path('admin-home/unblock_user/<int:user_id>/', views.unblock_user, name='unblock_user'),
 ]
--- environment/razopy/admins/views.py
+++ environment/razopy/admins/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import auth, messages
+from django.db.models import Q
+from django.apps import apps
+
+Account = apps.get_model('accounts', 'Account')
 
 # Create your views here.
 
@@ -8,8 +12,17 @@ def admin_home(request):
     return render(request, 'admins_temp/admin-home.html')
 
 
+
+#User Controls
 def admin_user_control(request):
-    return render(request, 'admins_temp/admin-user.html')
+    if 'q' in request.GET:
+        q = request.GET['q']
+        data = Account.objects.filter(Q(username__icontains=q) | Q(email__icontains=q))
+    else:
+        data = Account.objects.all()
+
+    context = {'data': data,}
+    return render(request, 'admins_temp/admin-user.html', context)
 
 
 def admin_wallet(request):
@@ -20,11 +33,13 @@ def admin_base(request):
     return render(request, 'admins_temp/admin-base.html')
 
 
+
 #Admin logout
 def admin_logout(request):
     auth.logout(request)
     messages.success(request,'you are logged out')
     return redirect('admin_login')
+
 
 
 #Admin login
@@ -43,3 +58,22 @@ def admin_login(request):
             return redirect('admin_login')
     else:
         return render(request, 'admins_temp/admin-login.html')
+    
+
+
+
+#Block user
+def block_user(request, user_id):
+    user = Account.objects.get(id=user_id)
+    user.is_active = False
+    user.save()
+    return redirect('admin_user_control')
+
+
+
+#UnBlock user
+def unblock_user(request, user_id):
+    user = Account.objects.get(id=user_id)
+    user.is_active = True
+    user.save()
+    return redirect('admin_user_control')
