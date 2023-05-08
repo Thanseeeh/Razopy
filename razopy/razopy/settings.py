ff02751d8ebf5457b002db617955b04eabd9bc20
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-05-08 18:49:32 +0530

    settings restored

--- razopy/razopy/settings.py
+++ razopy/razopy/settings.py
@@ -130,11 +130,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
 
-STATIC_ROOT = '/home/ubuntu/Razopy/razopy/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# STATIC_ROOT = '/home/ubuntu/Razopy/razopy/'
 
 MEDIA_URL = '/uploads/'
 
