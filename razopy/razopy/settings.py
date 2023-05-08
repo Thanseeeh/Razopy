ad0980a08d239e2137c9adef8036c0f4f0ba4006
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-05-08 18:41:26 +0530

    static settings

--- razopy/razopy/settings.py
+++ razopy/razopy/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -135,7 +134,7 @@ STATIC_URL = 'static/'
 #     os.path.join(BASE_DIR, 'static')
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/ubuntu/Razopy/razopy/'
 
 MEDIA_URL = '/uploads/'
 
